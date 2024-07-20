a1626a128020e2e522121862404f0dfe0698ed6f
TheMostUpset <themostupset@gmail.com>
2024-07-20 12:17:10 +0500

    upd changelog

--- gamemodes/hl1coop/gamemode/shared.lua
+++ gamemodes/hl1coop/gamemode/shared.lua
@@ -70,6 +70,7 @@ GM.Changelog = [[- Added 1 HP mode
 - Added nicknames for connecting players
 - Added player nickname on their weaponbox
 - Added ability to respawn for score in survival mode
+- Added new viewbob style: Serious Sam
 - Added hl1coop_npcrepl_ichthyosaur convar
 - Added hl1_coop_sv_waitcloak convar
 - Added Break input and OnBreak output on func_pushable
